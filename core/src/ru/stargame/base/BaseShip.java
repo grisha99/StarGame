2fcf4429909531dec4b3c36fe5ce30c4598e0bb5
Grishenko <grishenko_sy@mail.ru>
2020-10-20 11:44:56 +0300

    finish homework two with fix

--- core/src/ru/stargame/base/BaseShip.java
+++ core/src/ru/stargame/base/BaseShip.java
@@ -8,10 +8,9 @@ public class BaseShip {
     private Texture texture;            // изображение объекта
     private Vector2 position;           // текущая позиция
     private float speed;                // скорость передвижения
-    private Vector2 vSpeed;             // вектор скорости (не используется....пока)
     private Vector2 direction;          // направление движения (нормализована)
     private Vector2 endPosition;        // конечноая точка
-    private Vector2 nextPosition;       // следующая координата для перемещения
+    private Vector2 tmp;
 
     public BaseShip(String texturePath, float startX, float startY, float speed) {
         texture = new Texture(texturePath);
@@ -19,17 +18,16 @@ public class BaseShip {
         this.speed = speed;
         endPosition = position.cpy();
         direction = new Vector2();
-        nextPosition = new Vector2(0, 0);
+        tmp = new Vector2();
     }
 
     public void move() {
-        if (position.cpy().sub(endPosition).len() > speed) {        // конечная точка не достигнута
-            nextPosition.set(direction.cpy().scl(speed));           // сл.позиция с учетом скорости
-        } else {                                                    // объект достиг конечной точки
-            nextPosition.setZero();                                 // сл.позиция нулевая (нет перемещения)
-            position.set(endPosition);                              // ровняем текущую позицию с конечной (для точности)
+        tmp.set(endPosition);
+        if (tmp.sub(position).len() >= direction.len()) {       // конечная точка не достигнута
+            position.add(direction);                            // сл.позиция
+        } else {                                                // объект достиг конечной точки
+            position.set(endPosition);                          // ровняем текущую позицию с конечной (для точности)
         }
-        position.add(nextPosition);                                 // перемещаем объект на след. позицию
     }
 
     public Texture getTexture() {
@@ -54,10 +52,6 @@ public class BaseShip {
 
     public void setEndPosition(float x, float y) {
         endPosition.set(x - (texture.getWidth() / 2f), y - (texture.getHeight() / 2f)); // конечная позиция с учетом размеров объекта
-        direction = endPosition.cpy().sub(position).nor();                              // расчет направления движения до конечной позиции
-    }
-
-    public Vector2 getNextPosition() {
-        return nextPosition;
+        direction = endPosition.cpy().sub(position).setLength(speed);                   // расчет направления движения до конечной позиции с учетом скорости
     }
 }
